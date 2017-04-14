fd2ff1d9703ec94dd4d11fa20a2b13c0992a90e5
sherminb <Sherea@apples-MacBook-Pro.local>
2017-04-14 11:30:04 -0600

    Initial Commit

---
+++ pokefinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokefinder
+//
+//  Created by Kuala on 2017-04-14.
+//  Copyright © 2017 Litroom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
