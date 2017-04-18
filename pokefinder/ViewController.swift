46aea4ed0a9b0aefdafffb996fb81647c840cca8
sherminb <Sherea@apples-MacBook-Pro.local>
2017-04-18 14:50:30 -0600

    Setting up location manager

--- pokefinder/ViewController.swift
+++ pokefinder/ViewController.swift
@@ -7,21 +7,76 @@
 //
 
 import UIKit
+import MapKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDelegate {
+
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var mapCeneterd = false//center the map on load
+    
     var geoFire: GeoFire!
+    var geoFireRef : FIRDatabaseReference!//reference to the firebase db that geofire uses for saving pokemon locations
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = .follow
+        
+        
+        geoFireRef = FIRDatabase.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireRef)
+        
     }
-
+    func createSighting(forLocation location:CLLocation, withPokemon pokeId: Int){
+        geoFire.setLocation(location, forKey: "\(pokeId)")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    func locationAuthStatus(){
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            mapView.showsUserLocation = true
+        }else{
+            
+        }
+    }
+    func centerMap(location: CLLocation){
+        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        mapView.setRegion(region, animated: true)
+    }
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let location = userLocation.location{
+            if !mapCeneterd{
+                centerMap(location: location)
+                mapCeneterd = true
+            }
+        }
+    }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var newAnnotation : MKAnnotationView?
+        if annotation.isKind(of: MKUserLocation.self){
+            newAnnotation = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            newAnnotation?.image =  UIImage(named: "ash")
+        }
+        return newAnnotation
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func spotPokeman(_ sender: Any) {
+    }
 
 }
 
